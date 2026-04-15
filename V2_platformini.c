db4df140c7e52d4ff8c8f2d571c9d2db2007d44b
SSilicone <williams.silasm@gmail.com>
2026-04-15 18:56:49 -0400

    libraries, flags, operating specs, etc.

---
+++ V2_platformini.c
@@ -0,0 +1,24 @@
+[env:esp32dev]
+platform  = espressif32
+board     = esp32dev
+framework = arduino
+monitor_speed  = 115200
+upload_speed   = 921600
+upload_port    = /dev/ttyUSB0
+monitor_port   = /dev/ttyUSB0
+monitor_filters = esp32_exception_decoder
+board_build.partitions = huge_app.csv
+board_build.arduino.memory_type = qio_qspi   ; ← enables PSRAM
+build_flags =
+    -DCORE_DEBUG_LEVEL=4
+    -DCONFIG_BT_ENABLED
+    -DCONFIG_CLASSIC_BT_ENABLED
+    -DCONFIG_A2DP_ENABLE
+    -DBOARD_HAS_PSRAM              
+    -mfix-esp32-psram-cache-issue   ;(fixes PSRAM cache bug on rev1 silicon)
+build_unflags =
+    -DCONFIG_BT_BLE_ENABLED
+lib_deps =
+    fastled/FastLED @ ^3.6.0
+    kosme/arduinoFFT @ ^2.0.1
+    https://github.com/pschatzmann/ESP32-A2DP.git#v1.8.4
